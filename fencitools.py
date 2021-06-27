094036ed89447069ff950204737bb29350cd469f
Tanchengyong <86539379+Tanchengyong@users.noreply.github.com>
2021-06-27 18:43:41 +0800

    Add files via upload

--- fencitools.py
+++ fencitools.py
@@ -1,20 +1,22 @@
-import os,sys   # 轻量级数据库
-
+import os,sys ,time,re
+from tkinter import messagebox
 import jieba         #分词python库
 from matplotlib import pyplot    #python画图库
-# from wordcloud import STOPWORDS as stop
-from wordcloud import WordCloud  #python词云库
+import collections
+from wordcloud import WordCloud,STOPWORDS #python词云库
 from PIL import Image    # 图形处理，图形序化、验证码
 import numpy  as np      #矩阵数据处理
-
+import cipin
 
 
 class createFCpng():
-    def __init__(self, filePath=None, maskPicture=None,savePath=None,OneWords = None):
+    def __init__(self,window, log_text,filePath=None, maskPicture=None,savePath=None,OneWords = None):
         self.filePath = filePath
         self.maskPicture = maskPicture
         self.savePath = savePath
         self.OneWords = OneWords
+        self.log_text = log_text
+        self.window = window
         self.wcConfig = {
             "background_color" : "white",
                     "contour_width" :  0,
@@ -22,50 +24,120 @@ class createFCpng():
                     "contour_color" : 'steelblue',
                     "repeat"  :   True
         }
+        
+    def insertLog(self,msg):
+        self.log_text.config(state="normal")
+        self.log_text.insert("end",msg)
+        self.log_text.config(state="disabled")
+        self.window.update() 
+        
+        
+    def del_words(self):
+        
+        words = ['的','地','要','和','它','与',"一"]
+        self.cipinWords = [ i for i in self.cutWordsAll if i not in words and len(i) >= 2]
+        self.cutWords = " ".join(self.cipinWords)
+        print(self.cutWords)
+        
+    def get_current_time(self):
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        return current_time
+    
     def getWords(self):
         with open(self.filePath,"r",encoding="UTF-8") as reader:
-            self.text  =  reader.read()      
+            text  =  reader.read()
+            self.text = re.sub(r"\t|\n|\。|\、|\，|-|:|\（|\{|\}|\）|\?|\“|\”|\]|\[","",text)
+            
     def getOneWords(self):
-             self.text = str(self.OneWords)*300
+             self.text = str(self.OneWords)*500
+             
+             
     def cutWords(self):
-        self.cutWords =" ".join( jieba.cut(self.text)) 
-    def getMaskPicture(self):
+        msg = str(self.get_current_time()) +" " + str("正在进行分词,马上就好*_*-0.0 ....") + "\n"      #换行
+        self.insertLog(msg) 
         
+        self.cutWordsAll =jieba.cut(self.text) # 没有去掉无用的词
+        print("dsadadddddddddddd")
+        self.del_words()
+        cipin.ciping = collections.Counter(self.cipinWords).most_common(10)
+        
+        msg = str(self.get_current_time()) +" " + str("分词成功！") + "\n"      #换行
+        self.insertLog(msg) 
+        
+    def getMaskPicture(self):
+        msg = str(self.get_current_time()) +" " + str("正在处理图片,马上就好*_*-0.0 ....") + "\n"      #换行
+        self.insertLog(msg) 
         maskPicture = Image.open(self.maskPicture)
         self.maskPictureArray = np.array(maskPicture)
+        msg = str(self.get_current_time()) +" " + str("图片处理成功！") + "\n"      #换行
+        self.insertLog(msg)
+        
     def generateWC(self):
-            # stopwordss = set(stop)
-            # stopwordss.add("said")
+            stopwordss = set(STOPWORDS)
+            stopwordss.add("said")
             self.wcConfig["mask"] = self.maskPictureArray
-            # self.wcConfig["stopwords"] = stopwordss
+            self.wcConfig["stopwords"] = stopwordss
             self.wc = WordCloud(
                     **self.wcConfig
                 )
             self.wc.generate_from_text(self.cutWords)
-    def drawPinture(self):
+            
+    def drawPinture(self,saveFlag=None):
         # pyp = pyplot.figure(1)
 
-        pyplot.imshow(self.wc)
+        if saveFlag:
+            if self.savePath:
+                msg = str(self.get_current_time()) +" " + str("正在保存词云图片,马上就好 *_*-0.0  ....") + "\n"      #换行
+                self.insertLog(msg)
+                pyplot.imshow(self.wc)
+                pyplot.axis("off")
+                pyplot.savefig(self.savePath + self.fileName,dpi=300)
+                msg = str(self.get_current_time()) +" " + str("保存成功：保存路径：{}".format(self.savePath + self.fileName)) + "\n"*2      #换行
+                self.insertLog(msg)
+                messagebox.showinfo("提示","保存成功，路径为{}".format(self.savePath + self.fileName))  
+            else:
+                messagebox.showinfo("提示","保存路径不能为空")
+                
+        else:
+            try:
+                msg = str(self.get_current_time()) +" " + str("正在生成预览图片,马上就好*_*-0.0 ....") + "\n"      #换行
+                self.insertLog(msg)
+                pyplot.imshow(self.wc)
 
-        pyplot.axis("off")
+                pyplot.axis("off")
 
-        pyplot.show()
-        pyplot.imshow(self.wc)
-        pyplot.axis("off")
-        pyplot.savefig(self.savePath + self.fileName,dpi=300)
+                pyplot.show()
+                msg = str(self.get_current_time()) +" " + str("预览图片生成 成功！") + "\n"*2      #换行
+                self.insertLog(msg)
+
+            except Exception as e:
+                msg = str(self.get_current_time()) +" " + str("预览图片生成 失败！：：{}".format(e)) + "\n"*2      #换行
+                self.insertLog(msg)
+
     
-    def  starter(self):
+    def  starter(self,saveFlag=None):
         
         if self.OneWords:
-            self.fileName = "FenCi.png"
+            self.fileName = r"/FenCi.png"
             self.getOneWords()   
         if self.filePath:
-            self.fileName = "\\" + self.filePath.split(".")[0].split("/")[-1:][0] + ".png"
+            self.fileName = "/" + self.filePath.split(".")[0].split("/")[-1:][0] + ".png"
             print(self.fileName)
             self.getWords()
         self.cutWords()    
         self.getMaskPicture()    
         self.generateWC()    
-        self.drawPinture()    
+        self.drawPinture(saveFlag)
+    
+    
+    def setCiPin(self):
+        if self.OneWords:
+            self.fileName = r"/FenCi.png"
+            self.getOneWords()   
+        if self.filePath:
+            self.fileName = "/" + self.filePath.split(".")[0].split("/")[-1:][0] + ".png"
+            print(self.fileName)
+            self.getWords()
+        self.cutWords() 
         
-# createFCpng("text.txt","xin.png",r".\ciyu_ma.png").starter()
+        
--- mainGui.py
+++ mainGui.py
@@ -1,84 +1,203 @@
 
+import re
 from tkinter import filedialog
-import os,sys
+import os,sys,time
 
 from tkinter import *
+
+from matplotlib.pyplot import text
 from fencitools import createFCpng
-class MY_GUI():
-    def __init__(self,init_window_name):
-        self.init_window_name = init_window_name
+from tkinter import messagebox
+import cipin
+
+class mainGui():
+    def __init__(self,window):
+        self.window = window
         
     def set_init_window(self):
-        self.init_window_name.title("词云图片生成器")           #窗口名
-        #self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
-        self.init_window_name.geometry('800x400+300+100')
-        self.init_window_name["bg"] = '#%02x%02x%02x'%(2,3,25)                                   #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
-        #self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
-       
+        self.window.title("词云图片生成器")           #窗口名
+        #self.window.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
+        self.window.geometry('750x550+300+60')
+        self.window["bg"] = 'green'                                   #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
+        #self.window.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
        
         #选择词云分析文件
-        self.init_data_label = Label(self.init_window_name, text="  词云分析文件路径：")
+        self.init_data_label = Label(self.window, text="解析分析文件：\n(只能为txt格式)")
         self.init_data_label.grid(row=1, column=2,padx=15)
         
-        self.chooseFilePath = StringVar(self.init_window_name)
-        self.entry_chooseFilePath = Entry(self.init_window_name,textvariable=self.chooseFilePath, width=66)  
+        self.chooseFilePath = StringVar(self.window)
+        self.entry_chooseFilePath = Entry(self.window,textvariable=self.chooseFilePath, width=66)  
         self.entry_chooseFilePath.grid(row=1, column=3,  columnspan=10,pady=15,padx=6)
         
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="选择文件", bg="lightblue", width=10,command=self.chooseFile)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=1, column=16,pady=15,padx=15)
+        self.str_trans_to_md5_button = Button(self.window, 
+                                              text="选择文件", 
+                                              bg="yellow", 
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=10,
+                                              command=self.chooseFile)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=1, column=13,pady=15,padx=15)
         
         #选择词云模型图片
-        self.init_data_label = Label(self.init_window_name, text="  模型图片路径：")
+        self.init_data_label = Label(self.window, text="模型图片：")
         self.init_data_label.grid(row=2, column=2,padx=15)
         
-        self.choosePicturePath = StringVar(self.init_window_name)
-        self.entry_choosePicturePath = Entry(self.init_window_name,textvariable=self.choosePicturePath, width=66)  
+        self.choosePicturePath = StringVar(self.window)
+        self.entry_choosePicturePath = Entry(self.window,textvariable=self.choosePicturePath, width=66)  
         self.entry_choosePicturePath.grid(row=2, column=3,  columnspan=10,pady=15,padx=6)
         
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="选择模型图片", bg="lightblue", width=10,command=self.choosePicture)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=2, column=16,pady=15,padx=15)
+        self.str_trans_to_md5_button = Button(self.window,
+                                              text="选择模型图片",
+                                              bg="yellow",
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=10,
+                                              command=self.choosePicture)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=2, column=13,pady=15,padx=15)
         
     
         #或者自定义分词
-        self.log_label = Label(self.init_window_name, text="自定义分词内容：\n(自定义分词内容时上面路径不需选择)")
-        self.log_label.grid(row=13, column=2)
+        self.contain_label = Label(self.window, text="自定义分词内容：\n(自定义分词内容时\n上面路径不需选择)")
+        self.contain_label.grid(row=13, column=2)
        
-        self.contain_data_Text = Text(self.init_window_name, width=66, height=9)  
+        self.contain_data_Text = Text(self.window, 
+                                      width=66,
+                                      bg="lightyellow",
+                                      height=9
+                                      )  
         self.contain_data_Text.grid(row=13, column=3, columnspan=10,pady=15,padx=6)
+        
+        self.btn_seeCiPin = Button(self.window,
+                                              text="查看词频统计",
+                                              bg="yellow", 
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=10,
+                                              command=self.seeCiPin)  # 调用内部方法  加()为直接调用
+        self.btn_seeCiPin.grid(row=13, column=13,pady=15,padx=15)
        
         
         #选择保存路径
-        self.init_data_label = Label(self.init_window_name, text="  选择保存路径：")
+        self.init_data_label = Label(self.window, text="保存路径：")
         self.init_data_label.grid(row=14, column=2,padx=15)
         
-        self.set_saveFilePath = StringVar(self.init_window_name)   #动态赋值
-        self.enry_saveFilePath = Entry(self.init_window_name,textvariable=self.set_saveFilePath, width=66)  
+        self.set_saveFilePath = StringVar(self.window)   #动态赋值
+        self.enry_saveFilePath = Entry(self.window,textvariable=self.set_saveFilePath, width=66)  
         self.enry_saveFilePath.grid(row=14, column=3,  columnspan=10,pady=15,padx=6)
         
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="选择保存路径", bg="lightblue", width=10,command=self.saveFile)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=14, column=16,pady=15,padx=15)
+        self.btn_saveFilePath = Button(self.window,
+                                              text="选择保存路径",
+                                              bg="yellow", 
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=10,
+                                              command=self.saveFile)  # 调用内部方法  加()为直接调用
+        self.btn_saveFilePath.grid(row=14, column=13,pady=15,padx=15)
         
+        # 日志
         
+        self.log_text = Text(self.window,width=77,height=13,bg="lightgray")
+        self.log_text.insert("end","操作日志：\n")  
+        self.log_text.config(state=DISABLED)
+        self.log_text.grid(row=15, column=0,rowspan=2,  columnspan=13,pady=15,padx=10)
         #按钮
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="生成词云图片", bg="lightblue", width=10,command=self.generatePicture)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=15, column=4)
-        self.str_trans_to_md5_button = Button(self.init_window_name, text="预览", bg="lightblue", width=10,command=self.generatePicture)  # 调用内部方法  加()为直接调用
-        self.str_trans_to_md5_button.grid(row=15, column=8)
+        
+        self.str_trans_to_md5_button = Button(self.window,
+                                              text="预览", 
+                                              bg="lightblue",
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=13,
+                                              height=2,
+                                              command=self.generatePicture)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=15, column=13)
+        
+        self.str_trans_to_md5_button = Button(self.window,
+                                              text="保存", 
+                                              bg="lightblue",
+                                              activebackground = "pink",
+                                              activeforeground = "blue",
+                                              width=13,
+                                              height=2,
+                                              command=self.savePicture,
+                                              relief=GROOVE)  # 调用内部方法  加()为直接调用
+        self.str_trans_to_md5_button.grid(row=16, column=13)
         
         
         
+    
+             
+    
+    
+    def getValues(self):
+        self.filePath = self.entry_chooseFilePath.get()
+        self.picturePath = self.entry_choosePicturePath.get()
+        self.context = self.contain_data_Text.get(1.0,END).strip().replace("\n","")
+        self.savePath = self.enry_saveFilePath.get()
+    
+    def createFcPngObject(self,flag=None):
+        if flag == "OneWords":
+            self.createFCpng = createFCpng(self.window,
+                                                self.log_text,
+                                                OneWords=self.context,
+                                                maskPicture=self.picturePath
+                                                )
+        else:
+            print(self.savePath)
+            self.createFCpng = createFCpng(self.window,
+                                                self.log_text,
+                                                filePath= self.filePath,
+                                                maskPicture=self.picturePath,
+                                                savePath=self.savePath)
     def generatePicture(self):
-         filePath = self.entry_chooseFilePath.get()
-         picturePath = self.entry_choosePicturePath.get()
-         context = self.contain_data_Text.get(1.0,END).strip().replace("\n","")
-         savePath = self.enry_saveFilePath.get()
-         if context:
-            self.createFCpng = createFCpng(OneWords=context,maskPicture=picturePath,savePath=savePath)
-            self.createFCpng.starter()
-         if filePath:
-            self.createFCpng = createFCpng(filePath=filePath,maskPicture=picturePath,savePath=savePath)
-            self.createFCpng.starter()
-
+         
+         self.getValues()
+         if (self.context or self.filePath) and self.picturePath:
+            try:
+                if self.context:
+                    self.createFcPngObject("OneWords")
+                    self.createFCpng.starter()
+                if self.filePath:
+                    if ".txt" in self.filePath:
+                        self.createFcPngObject()
+                        self.createFCpng.starter()
+                    else:
+                        messagebox.showwarning("警告","解析文件目前只支持txt格式")
+            except Exception as e:
+                if "utf-8" in str(e):
+                    msg = str(self.get_current_time()) +" " + str("发生异常,文件编码格式不对，请操作‘另存为’文件并设置编码格式为UTF8") + "\n"*3      #换行
+                    self.insertLog(msg)
+         else:
+             messagebox.showwarning("警告","解析文件路径或者自定义词语任选其一，模板图片必填")
+    
+    
+    def savePicture(self): 
+        self.getValues()
+        if (self.context or self.filePath) and self.picturePath and self.savePath:
+            try:
+                if self.context:
+                    self.createFcPngObject("OneWords")
+                    self.createFCpng.starter()
+                if self.filePath:
+                    self.createFcPngObject()
+                    self.createFCpng.starter(saveFlag=True)
+            except Exception as e:
+                if "utf-8" in str(e):
+                    msg = str(self.get_current_time()) +" " + str("发生异常,文件编码格式不对，请操作‘另存为’文件并设置编码格式为UTF8") + "\n"*3      #换行
+                    self.insertLog(msg) 
+        else:
+            messagebox.showinfo("提示","解析文件路径或者自定义词语任选其一，模板图片、保存路径必填")
+    
+    def get_current_time(self):
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        return current_time  
+     
+    def insertLog(self,msg):
+        self.log_text.config(state=NORMAL)
+        self.log_text.insert("end",msg)
+        self.log_text.config(state=DISABLED)
+        self.window.update()   
+       
     def chooseFile(self):
         default_dir = r"C:\Users\lenovo\Desktop"  # 设置默认打开目录
         fname = filedialog.askopenfilename(title=u"选择文件", initialdir=(os.path.expanduser(default_dir)))
@@ -94,13 +213,43 @@ class MY_GUI():
         fname = filedialog.askdirectory(title=u"选择文件", initialdir=(os.path.expanduser(default_dir)))
         self.set_saveFilePath.set(fname)
     
+    def seeCiPin(self):
+        self.getValues()
+        if (self.context or self.filePath) and self.picturePath:
+            try:
+                if self.context:
+                    self.createFcPngObject("OneWords")
+                    self.createFCpng.setCiPin()
+                if self.filePath:
+                    if ".txt" in self.filePath:
+                        self.createFcPngObject()
+                        self.createFCpng.setCiPin()
+                    else:
+                        messagebox.showwarning("警告","解析文件目前只支持txt格式")
+                ci = [str(i) for i in cipin.ciping]
+                messagebox.showwarning("只展示前10个","\n".join(ci))
+            except Exception as e:
+                if "utf-8" in str(e):
+                    msg = str(self.get_current_time()) +" " + str("发生异常,文件编码格式不对，请操作‘另存为’文件并设置编码格式为UTF8") + "\n"*3      #换行
+                    self.insertLog(msg)
+        else:
+             messagebox.showwarning("警告","解析文件路径或者自定义词语任选其一，模板图片必填")
+             
+        
+        
+        
+    
+        # 
+    
+        
+    
     def del_object(self):
         sys.exit(0)
         
         
 def gui_start():
     init_window = Tk()              #实例化出一个父窗口
-    ZMJ_PORTAL = MY_GUI(init_window)
+    ZMJ_PORTAL = mainGui(init_window)
     # 设置根窗口默认属性
     ZMJ_PORTAL.set_init_window()
 
